1c54ddeb8953a7bff0acae0cdee33e76e406a251
hussein <shtam.hareb12@gmail.com>
2024-04-13 15:57:40 +0300

    space logic

--- src/ui.rs
+++ src/ui.rs
@@ -14,7 +14,8 @@ pub fn listen_for_alphabets() {
     let mut stdout = io::stdout().into_raw_mode().expect("Failed to set raw mode");
 
     let mut i = 0;
-    let mut char_status: Vec<char> = vec!['N'; initial_text.len()]; // 'N' means neutral, 'T' means true (correct), 'F' means false (incorrect)
+     // 'N' means neutral, 'T' means true (correct), 'F' means false (incorrect)
+    let mut char_status: Vec<char> = vec!['N'; initial_text.len()];
     let mut colored_text = String::new(); 
 
     println!("{}", initial_text);
@@ -28,13 +29,12 @@ pub fn listen_for_alphabets() {
                             i -= 1;
                             char_status[i] = 'N';
 
-                            // Regenerate colored text
                             colored_text.clear();
                             for (index, char) in initial_text.chars().enumerate() {
                                 match char_status[index] {
-                                    'N' => colored_text.push_str(WHITE),
-                                    'T' => colored_text.push_str(GREEN),
-                                    'F' => colored_text.push_str(RED),  
+                                    'N' => colored_text.push_str(WHITE), // Neutral (not reached yet) is white
+                                    'T' => colored_text.push_str(GREEN), // True (correctly matched) is green
+                                    'F' => colored_text.push_str(RED),   // False (incorrectly matched) is red
                                     _ => {}
                                 }
                                 colored_text.push(char);
@@ -43,33 +43,37 @@ pub fn listen_for_alphabets() {
                             print!("{}{}", termion::cursor::Goto(1, 1), colored_text); // Move cursor to beginning of line before printing
                         }
                     }
-                    termion::event::Key::Char(c) if c.is_alphabetic() => {
-                        print!("{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), termion::cursor::Hide);
-                        io::stdout().flush().unwrap();
-
-                        if c == initial_text.chars().nth(i).unwrap() {
-                            char_status[i] = 'T';
-                        } else {
-                            char_status[i] = 'F';
+                    termion::event::Key::Char(c) => {
+                        if c == ' ' {
+                            i += 1;
+                            continue; 
                         }
+                        if c.is_alphabetic() {
+                            print!("{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), termion::cursor::Hide);
+                            io::stdout().flush().unwrap();
 
-                        // Regenerate colored text
-                        colored_text.clear();
-                        for (index, char) in initial_text.chars().enumerate() {
-                            match char_status[index] {
-                                'N' => colored_text.push_str(WHITE), // Neutral (not reached yet) is white
-                                'T' => colored_text.push_str(GREEN), // True (correctly matched) is green
-                                'F' => colored_text.push_str(RED),   // False (incorrectly matched) is red
-                                _ => {}
+                            if c == initial_text.chars().nth(i).unwrap() {
+                                char_status[i] = 'T';
+                            } else {
+                                char_status[i] = 'F';
                             }
-                            colored_text.push(char);
-                        }
-                        colored_text.push_str(WHITE);
-                        println!("{}", colored_text);
 
-                        i += 1; // Increment i for alphabetic keys
+                            colored_text.clear();
+                            for (index, char) in initial_text.chars().enumerate() {
+                                match char_status[index] {
+                                    'N' => colored_text.push_str(WHITE), 
+                                    'T' => colored_text.push_str(GREEN), 
+                                    'F' => colored_text.push_str(RED),   
+                                    _ => {}
+                                }
+                                colored_text.push(char);
+                            }
+                            colored_text.push_str(WHITE);
+                            println!("{}", colored_text);
+
+                            i += 1; 
+                        }
                     }
-                    // Handle other key presses
                     _ => {}
                 }
             }
