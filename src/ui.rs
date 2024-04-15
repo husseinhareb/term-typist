3df638fd12ec4c3c1d5781420286a2c99606ec83
hussein <shtam.hareb12@gmail.com>
2024-04-15 19:36:08 +0300

    optimizing fun

--- src/ui.rs
+++ src/ui.rs
@@ -1,11 +1,11 @@
 use std::io::{self, Write};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use std::time::{Instant, SystemTime};
+
 use crate::generator::generate_random_sentence;
 use crate::config::read_nb_of_words;
 use crate::wpm::elapsed_seconds_since_start;
-
+use termion::clear;
 const GREEN: &str = "\x1b[32m";
 const RED: &str = "\x1b[31m";
 const WHITE: &str = "\x1b[0m"; 
@@ -23,22 +23,18 @@ pub fn listen_for_alphabets() {
     let mut stdout = io::stdout().into_raw_mode().expect("Failed to set raw mode");
 
     let mut i = 0;
+    let mut char_count = 0;
     let mut char_status: Vec<char> = vec!['N'; initial_text.len()];
     let mut colored_text = String::new();
-    let mut last_key_time = Instant::now();
-    let mut char_count = 0;
 
+    print!("{}{}", clear::All, termion::cursor::Goto(1, 1));
+    stdout.flush().unwrap();
     println!("{}", initial_text);
 
-    let start_time = SystemTime::now(); // Capture the start time
 
     for key in stdin.keys() {
         match key {
             Ok(key_event) => {
-                let now = Instant::now();
-                let elapsed = now - last_key_time;
-                last_key_time = now;
-
                 match key_event {
                     termion::event::Key::Backspace => {
                         if i > 0 {
@@ -83,13 +79,6 @@ pub fn listen_for_alphabets() {
                 }
                 colored_text.push_str(WHITE);
                 print!("{}{}{}", termion::clear::All, termion::cursor::Goto(1, 1), colored_text); // Clear terminal and move cursor to beginning   
-
-                let elapsed_seconds = elapsed_seconds_since_start(start_time); // Pass start time
-                let word_count = char_count / 5; // Assuming average word length is 5 characters
-                let elapsed_minutes = elapsed_seconds / 60.0;
-                let wpm = (word_count as f64) / elapsed_minutes;
-
-                println!(" Speed: {:.2} words per minute", wpm); // Print words per minute
                 io::stdout().flush().unwrap();      
             }
             Err(err) => {
